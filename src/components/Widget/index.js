6f4f3abac403e19fdbb687d8810623adb9b7522d
Mário André <mario_andre.l.a@hotmail.com>
2021-01-29 22:39:14 -0300

    configurado css das opções do quiz, criado logica de questoes certas e erradas e questoes acertadas. E usado nome do usuario no parametro da URL no componente resultado

--- src/components/Widget/index.js
+++ src/components/Widget/index.js
@@ -1,7 +1,7 @@
 import styled from 'styled-components';
 
 const Widget = styled.div`
-  margin-top: 24px;
+  /* margin-top: 24px; */
   margin-bottom: 24px;
   border: 1px solid ${({ theme }) => theme.colors.contrastText};
   background-color: ${({ theme }) => theme.colors.mainBg};
@@ -48,21 +48,20 @@ Widget.Content = styled.div`
     list-style: none;
     padding: 0;
   }
-  /* input {
-    width: 100%;
-    border-radius: 5px;
-    background: none;
-    border: 1px solid ${({ theme }) => theme.colors.primary};
-    height: 2rem;
-    padding-left: 10px;
-    margin-bottom: 15px;
-    color: ${({ theme }) => theme.colors.contrastText}
-    
-  } */
-  /* .input-quiz-users {
-    margin-bottom: 0.5rem;
-    background: #272a52;
-  } */
+`;
+
+Widget.User = styled.div`
+  padding: 24px 32px 32px 32px;
+  & > *:first-child {
+    margin-top: 0;
+  }
+  & > *:last-child {
+    margin-bottom: 0;
+  }
+  ul {
+    list-style: none;
+    padding: 0;
+  }
 `;
 
 Widget.Topic = styled.a`
