4125e52f9e7858a27c2d23ac9420d2f940f444ac
Mário André <mario_andre.l.a@hotmail.com>
2021-02-01 11:18:55 -0300

    Atualizado link imagem em tag meta property=og:image

--- src/components/IndexPage/index.js
+++ src/components/IndexPage/index.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import Head from 'next/head';
 
 function IndexPage() {
@@ -7,7 +8,7 @@ function IndexPage() {
         <title>One Piece Quiz - Alura</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
         <link rel="shortcut icon" href="https://icons.iconarchive.com/icons/crountch/one-piece-jolly-roger/128/Luffys-flag-icon.png" />
-        <meta property="og:image" content="https://images5.alphacoders.com/641/641999.jpg" />
+        <meta property="og:image" content="https://static.zerochan.net/ONE.PIECE.full.691826.jpg" />
         <meta property="og:image:type" content="image/jpeg" />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet" />
